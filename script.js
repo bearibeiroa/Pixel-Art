1b0afb5054a537bf0963c0f167366c675f8a6a55
Beatriz Ribeiro <contato.biaalves@gmail.com>
2021-07-09 14:33:44 +0100

    Requisito 7

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ window.onload = function() {
 
     function createColorPalette() {
         const linePalette = document.querySelector('#color-palette');
-        let arrayOfColors = ['black', 'red', 'orange', 'green'];
+        const arrayOfColors = ['black', 'red', 'orange', 'green'];
         for (let index = 0; index < 4; index += 1) {
             const line = document.createElement('div');
             line.classList.add('color');
@@ -12,12 +12,14 @@ window.onload = function() {
     }
     createColorPalette();
 
-    let firstColor = document.querySelector('.color');
+
+    const firstColor = document.querySelector('.color');
     firstColor.classList.add('selected');
+
 
     function createPixelBoard(value) {
         const pixelBoardArea = document.querySelector('#pixel-board');
-        for (let i = 0; i < value; i += 1) {
+        for (let index = 0; index < value; index += 1) {
             const pixelItem = document.createElement('div');
             pixelItem.className = 'pixel';
             pixelBoardArea.appendChild(pixelItem);
@@ -25,5 +27,44 @@ window.onload = function() {
     }
     createPixelBoard(5 * 5);
 
+
+    function pickColor(color) {
+        console.log(color);
+        document.addEventListener('click', (event) => {
+            if (event.target.classList.contains('pixel')) {
+                event.target.style.backgroundColor = `${color}`;
+            }
+        }, false);
+    }
+    pickColor('orange')
+
+    function getPalleteColor() {
+        const pallete = document.querySelector('#color-palette');
+        const colors = document.querySelectorAll('.color');
+        pallete.addEventListener('click', (event) => {
+            colors.forEach((color) => {
+                color.classList.remove('selected');
+            });
+            if (event.target.classList.contains('color')) {
+                event.target.classList.add('selected');
+                pickColor(event.target.classList[1]);
+            } else {
+                colors[0].classList.add('selected');
+                pickColor('black');
+            }
+        });
+    }
+    getPalleteColor();
+
+    function clearBoardColors() {
+        const pixels = document.querySelectorAll('.pixel');
+        const button = document.querySelector('#clear-board');
+        button.addEventListener('click', () => {
+            pixels.forEach((pixel) => {
+                pixel.style.backgroundColor = 'white';
+            });
+        });
+    }
+    clearBoardColors();
 
 }
